02358d0244f5356fb3f4d73837c9e9b34d458c76
Andrysha <chavkin94@mail.ru>
2021-03-01 11:10:21 +0900

    Постарался от души

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+            Мы - КОМАНДА!
+        </h1>
+        <p>Занимаемся профессиональной разработкой сайтов. ТОП команда города Чита</p>
+        <p>С вопросами обращаться по ссылек FAQ</p>
+      </header>
+    </div>
+  );
+}
+
+export default About;
