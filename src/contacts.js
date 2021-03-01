65e9b25639fb7271caa38068658373a47a51f5f3
Your Name <you@example.com>
2021-03-01 11:06:16 +0900

    Contacts

---
+++ src/contacts.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Director from './Director.jpg';
+import Proger from './Proger.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Наши контакты</h1>
+        <h3>Генеральный директор</h3>
+        <h4>Чавкин Андрей Юрьевич</h4>
+        <img src={Director} className="App-logo" alt="Гинеральный директор" />
+        <h3>Главный специалист</h3>
+        <h4>Еремин Дмитрий Олегович</h4>
+        <img src={Proger} className="App-logo" alt="Главный специалист" />
+        <h3>Клоун</h3>
+        <h4>Сухачев Антон</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
