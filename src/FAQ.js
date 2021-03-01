620acfc5e5212658a27c6cc82af47465e7cf7457
dmitriy <dmitriy@mail.com>
2021-03-01 11:09:33 +0900

    Added FAQ component

---
+++ src/FAQ.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function FAQ() {
+    return (
+        <div>
+            <h3>FAQ</h3>
+            <ul>
+                <li>
+                    Кто мы такие?
+                    Мы команда разрабочиков
+                </li>
+                <li>
+                    Где мы находимся?
+                    USA
+                </li>
+                <li>
+                    Кто наш тимлид?
+                    Андрей Чавкин / Chavkin Andrew
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default FAQ;
