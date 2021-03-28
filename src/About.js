cca9903be87b5ed3e68fa944038e9fb58b485a04
chavkin94 <76653833+chavkin94@users.noreply.github.com>
2021-03-28 16:26:15 +0900

    Update About.js
    About

--- src/About.js
+++ src/About.js
@@ -1,15 +1,41 @@
 import './App.css';
 
+import db from './firebase.config';
+import React, {useEffect, useState} from "react";
+
 function About() {
+  const [Zagolovok, setZagolovok] = useState()
+  const [easyText, setEasyText] = useState([])
+
+  const fetchFaqs = () => {
+      db.collection("team-collection")
+          .doc('About')
+          .get()
+          .then(doc => {
+            setZagolovok(doc.data().Zagolovok)
+            setEasyText(doc.data().easyText)
+          });
+  }
+
+  useEffect(() => {
+      fetchFaqs()
+  }, [])
+
+
+  const AboutStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-            Мы - КОМАНДА!
-        </h1>
-        <p>Занимаемся профессиональной разработкой сайтов. ТОП команда города Чита</p>
-        <p>С вопросами обращаться по ссылек FAQ</p>
-      </header>
+    <div className="App" style={AboutStyle}>
+      {Zagolovok && Zagolovok.map(item => (
+                        <h1 key={item}>{item}</h1>
+                    ))}
+      {easyText && easyText.map(item => (
+                        <p key={item}>{item}</p>
+                    ))}
     </div>
   );
 }
